f8712d222516a25973278b8099d6518b1a3c9a5c
Carlos Lucas <nosk14@gmail.com>
2018-03-01 20:06:59 +0100

    Cars

--- driving.py
+++ driving.py
@@ -8,10 +8,29 @@ class Ride(object):
         self.end_point = (int(er), int(ec))
         self.start_time = int(st)
         self.end_time = int(et)
-        self.reward = abs(int(er)-int(sr)) + abs(int(ec)-int(sc))
+        self.distance = abs(int(er)-int(sr)) + abs(int(ec)-int(sc))
 
     def __str__(self):
         return str(self.id)
+
+
+class Car(object):
+    def __init__(self):
+        self.current_position = (0, 0)
+        self.current_time = 0
+        self.rides = []
+
+    def assign_ride(self, ride):
+        self.rides.append(ride)
+        self.current_time += self.get_timelapse(ride) + ride.distance
+        self.current_position = ride.end_point
+
+    def get_timelapse(self, ride):
+        distance = abs(ride.end_point[0]-self.current_position[0]) + abs(ride.end_point[1]-self.current_position[1])
+        now = self.current_time + distance
+        if ride.start_time > now:
+            now += ride.start_time - now
+        return now
 
 
 if __name__ == '__main__':
@@ -22,6 +41,8 @@ if __name__ == '__main__':
         for l in f.readlines():
             rides.append(Ride(i, *l[:-1].split(' ')))
             i += 1
+
+    cars = [Car() for _ in range(F)]
 
     for r in rides:
         print("{} -> {}".format(r, r.reward))
